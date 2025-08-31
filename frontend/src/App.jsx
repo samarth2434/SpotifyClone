1f7d5f963dc03093ce5894cd19a573423165de06
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2025-08-31 16:32:22 +0530

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from "react";
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import { UserData } from "./context/Users";
+import Loading from "./components/Loading";
+import Admin from "./pages/Admin";
+import PlayList from "./pages/PlayList";
+import Album from "./pages/Album";
 
 const App = () => {
-  return <div className="text-red-600">App</div>
-  
-}
+  const { loading, user, isAuth } = UserData();
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/playlist"
+              element={isAuth ? <PlayList user={user} /> : <Login />}
+            />
+            <Route
+              path="/album/:id"
+              element={isAuth ? <Album user={user} /> : <Login />}
+            />
+            <Route
+              path="/playlist"
+              element={isAuth ? <PlayList user={user} /> : <Login />}
+            />
+            <Route path="/admin" element={isAuth ? <Admin /> : <Login />} />
+            <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/register"
+              element={isAuth ? <Home /> : <Register />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { UserProvider } from "./context/user.jsx";
+import { SongProvider } from "./context/Song.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserProvider>
+      <SongProvider>
+        <App />
+      </SongProvider>
+    </UserProvider>
+  </StrictMode>
+);
