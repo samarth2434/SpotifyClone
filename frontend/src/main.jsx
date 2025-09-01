01346be4179bd049fba6f5ddfca05a4ff27cc761
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2025-09-01 17:58:46 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { UserProvider } from "./context/user.jsx";
+import { UserProvider } from "./context/User.jsx";
 import { SongProvider } from "./context/Song.jsx";
 
 createRoot(document.getElementById("root")).render(
